81eab9509f9549863b40322d7541a65d5bbf4752
Cory Jonet <CoryJonet@users.noreply.github.com>
2016-12-31 11:16:00 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export CLASSPATH=/p/course/cs536-loris/public/tools/deps_src/java-cup-11b.jar:/p/course/cs536-loris/public/tools/deps_src/java-cup-11b-runtime.jar:/p/course/cs536-loris/public/tools/deps:.
+export .
